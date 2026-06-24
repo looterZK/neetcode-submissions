d5bb29831121f45402c8f60854ba7fd497833dc6
looterZK <talhazubayeru20@gmail.com>
2026-06-24 09:54:04 -0400

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        while(i < nums.length - 1){
+            if(nums[i] == nums[i + 1]){
+                i +=2;
+            } else {
+                return nums[i];
+            }
+
+        }
+        return nums[i];
+    }
+}
